11e5074dc89bc7b53484e85ea6e4e86351217969
kimhyun <forhn37@gmail.com>
2023-10-08 18:31:16 +0900

    산술연산자 %는 나머지만을 구한다 헷갈리지 말자

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let time = 12000;
-let sec = '';
+// let time = 12000;
+// let sec = '';
 
 
-let limittime = setInterval(function() {
-  time--;
-  sec = time/60
-  document.getElementById('timehow').textContent= sec+'초'
-}, 12000);
+// let limittime = setInterval(function() {
+//   time--;
+//   sec = time/60
+//   document.getElementById('timehow').textContent= sec+'초'
+// }, 12000);
 
+
+
+console.log(1000%1000);
